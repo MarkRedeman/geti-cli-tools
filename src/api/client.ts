703dd56fb8d1b0d09570f8286408743843353942
Mark Redeman <markredeman@gmail.com>
2025-08-07 11:15:24 +0200

    Make clients more generic
    This would allow us to pass in different clients so that we can clone
    projects into different environments
    
    Later on this will also allow us to add middleware, for logging, error
    handling and caching.

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,13 +2,12 @@ import { createPathBasedClient } from 'openapi-fetch';
 import { getEnv } from './environment';
 import type { paths } from './geti-openapi-schema';
 
+export function getClient<Paths extends {} = paths>(baseUrl: string, apiKey: string) {
+    return createPathBasedClient<Paths>({ baseUrl, headers: { 'x-api-key': apiKey } });
+}
+
 const { baseUrl, apiKey } = getEnv();
 
-export const client = createPathBasedClient<paths>({
-    baseUrl,
-    headers: {
-        'x-api-key': apiKey,
-    },
-});
+export const client = getClient(baseUrl, apiKey); // client['version'] = '2.12';
 
 export type Client = typeof client;
--- src/api/environment.ts
+++ src/api/environment.ts
@@ -2,6 +2,14 @@ export function getEnv() {
     const baseUrl = process.env['BASE_URL'];
     const apiKey = process.env['API_KEY'];
 
+    if (baseUrl === undefined) {
+        throw new Error('Could not find BASE_URL environment variable');
+    }
+
+    if (apiKey === undefined) {
+        throw new Error('Could not find API_KEY environment variable');
+    }
+
     return {
         baseUrl,
         apiKey,
