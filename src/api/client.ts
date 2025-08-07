5ea00641de01d41fc400e3b9b7be7a6c9e318b4b
Mark Redeman <markredeman@gmail.com>
2025-08-07 12:49:20 +0200

    Allow to load client with different env keys

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,12 +2,16 @@ import { createPathBasedClient } from 'openapi-fetch';
 import { getEnv } from './environment';
 import type { paths } from './geti-openapi-schema';
 
-export function getClient<Paths extends {} = paths>(baseUrl: string, apiKey: string) {
+export function getClient<Paths extends {} = paths>({
+    baseUrl,
+    apiKey,
+}: {
+    baseUrl: string;
+    apiKey: string;
+}) {
     return createPathBasedClient<Paths>({ baseUrl, headers: { 'x-api-key': apiKey } });
 }
 
-const { baseUrl, apiKey } = getEnv();
-
-export const client = getClient(baseUrl, apiKey); // client['version'] = '2.12';
+export const client = getClient(getEnv());
 
 export type Client = typeof client;
